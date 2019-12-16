3091c0342033f0036e374da730613be9dbb85489
fvega <fer.vega@newsan.com.ar>
2019-12-16 06:35:48 -0300

    Chat finalizado v1

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -24,21 +24,26 @@ io.on('connection', (client) => {
 
         client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
 
+        client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${data.nombre} se unió`));
+
         callback(usuarios.getPersonasPorSala(data.sala));
     });
-    client.on('crearMensaje', (data) => {
-        let persona = usuarioto(data.para).s.getPersona(client.id);
+    client.on('crearMensaje', (data, callback) => {
+        let persona = usuarios.getPersona(client.id);
 
         let mensaje = crearMensaje(persona.nombre, data.mensaje);
 
         client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+        // client.broadcast.to(persona.sala).emit('crearMensaje', crearMensaje('Administrador', `${persona.nombre} se unió`));
+
+        callback(mensaje);
     });
 
     client.on('disconnect', () => {
         let personaBorrada = usuarios.borrarPersona(client.id, );
 
-        client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${personaBorrada.nombre}`));
-        client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala());
+        client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${personaBorrada.nombre} se deconecto...`));
+        client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala(personaBorrada.sala));
 
 
     });
